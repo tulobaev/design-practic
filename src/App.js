6837af938ca487ef531d629529ebeba81d87d538
suiorkulova <askmxxx7@gmail.com>
2024-01-20 14:54:13 +0600

    add task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import "./App.css";
-import Dose from "./components/Hero/Dose";
-import Hero from "./components/Hero/Hero";
-import Header from "./components/header/Header";
-import Section from "./components/section/Section";
-import SectionFirst from "./components/PartSection/SectionFirst";
-import SectionSecond from "./components/PartSection/SectionSecond";
-import SectionThird from "./components/PartSection/SectionThird";
-import Footer from "./components/Footer/Footer";
+import Main from "../src/Main"
+
 
 function App() {
   return (
     <div>
-      <div className="box">
-        <Header />
-        <Section />
-      </div>
-      <Hero />
-      <Dose />
-      <SectionFirst />
-      <SectionSecond />
-      <SectionThird />
-      <Footer />
+    <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,29 @@
+
+import "./App.css";
+import Dose from "./components/Hero/Dose";
+import Hero from "./components/Hero/Hero";
+import Header from "./components/header/Header";
+import Section from "./components/section/Section";
+import SectionFirst from "./components/PartSection/SectionFirst";
+import SectionSecond from "./components/PartSection/SectionSecond";
+import SectionThird from "./components/PartSection/SectionThird";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <div className="box">
+        <Header />
+        <Section />
+      </div>
+      <Hero />
+      <Dose />
+      <SectionFirst />
+      <SectionSecond />
+      <SectionThird />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
